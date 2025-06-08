44f79a5a3423b0fd27e0df3e9da42058f721bf0e
IhebRekik <162717252+IhebRekik@users.noreply.github.com>
2025-06-08 22:38:10 +0100

    Update Cas.jsx

--- src/services/Cas.jsx
+++ src/services/Cas.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 export const getCas = async()=>{
 
     try {
-        const response = await axios.get(`http://localhost:9999/utilisateur/getKey`);
+        const response = await axios.get(`https://serveurspring-hxd7d3gkc5ewcrfm.southafricanorth-01.azurewebsites.net/utilisateur/getKey`);
         return response.data;
       } catch (error) {
         console.error("Error fetching data: ", error);
